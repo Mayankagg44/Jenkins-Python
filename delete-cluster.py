7e3fc4c37d21f784bef8e7c537ee5aa286add8dd
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-13 15:29:34 +0530

    Create delete-cluster.py

---
+++ delete-cluster.py
@@ -0,0 +1,36 @@
+import boto3
+import sys
+
+status = sys.argv[1]
+list_db= sys.argv[2]
+client = boto3.client('rds')
+
+#Deleting RDS cluster
+def status_clusters_rds():
+    for db in list_db.split(","):
+        response = client.describe_db_clusters(
+            DBClusterIdentifier=db
+        )
+        for i in response['DBClusters']:
+            if status.lower() =='delete':
+                if i['Status'] == 'available':
+                    client.delete_db_cluster(DBClusterIdentifier = i['DBClusterIdentifier'])
+                    print('Deleting DB cluster {0}'.format(i['DBClusterIdentifier']))
+                elif i['Status'] == 'starting' or i['Status'] == 'stopping':
+                    print("It is in starting or stopping mode")
+                    sys.exit(1)
+                else :
+                    print('Wrong status')
+                    sys.exit(1)
+            # if status.lower() =='stop':
+            #     if i['Status'] == 'stopped' or i['Status'] == 'stopping':
+            #         print("Already stopped")
+            #         sys.exit(1)
+            #     elif i['Status'] == 'starting':
+            #         print('The DB cluster {0} is in starting mode...Kindly wait for few mins'.format(i['DBClusterIdentifier']))
+            #         sys.exit(1)
+            #     else:
+            #         client.delete_db_cluster(DBClusterIdentifier = i['DBClusterIdentifier'])
+            #         print('Deleting DB cluster {0}'.format(i['DBClusterIdentifier']))
+if __name__ == '__main__':
+   status_clusters_rds()
