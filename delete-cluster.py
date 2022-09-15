39a69d0949039f70102e04281ed032dd5db8b3ce
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-15 15:34:51 +0530

    Update delete-cluster.py

--- delete-cluster.py
+++ delete-cluster.py
@@ -55,47 +55,47 @@ def remove_global_clusters():
    
 # Recreate the RDS DB cluster(after it has been removed from the global database)
         time.sleep(120)
-        print("!!!!!!!!Creating the Cluster Now!!!!!!")
+        print("!!!!!!Creating the Cluster Now!!!!!!")
         # response = client.describe_db_clusters(DBClusterIdentifier=db_clu)
         #     print(response)
-        for j in response['DBClusters']:
-            if j['Status'] != 'available':
-                client.create_db_cluster(
-                    # DatabaseName='database-5',
-                    #CharacterSetName = 'string',
-                    DBClusterIdentifier=db_clu,
-                    DBClusterParameterGroupName='default.aurora-mysql5.7',
-                    VpcSecurityGroupIds=['default'],
-                    DBSubnetGroupName='db-subnet',
-                    Engine='aurora-mysql',
-                    EngineVersion='5.7.mysql_aurora.2.10.2',
-                    Port=3306,
-                    MasterUsername='admin',
-                    MasterUserPassword='password',
-                    OptionGroupName='default:aurora-mysql-5-7',
-                    ReplicationSourceIdentifier='arn:aws:rds:us-east-1:760451896171:cluster:database-3',
-                    StorageEncrypted=True,
-                    KmsKeyId='dfc76317-d847-4a42-b8c7-1c17ffadde02',
-                    EnableCloudwatchLogsExports=['general'],
-                    EngineMode='provisioned',
-                    EnableIAMDatabaseAuthentication=False,
-                    DeletionProtection=False,
-                    GlobalClusterIdentifier=list_db,
-                    #Domain='string',
+        # for j in response['DBClusters']:
+        #     if j['Status'] != 'available':
+        client.create_db_cluster(
+            # DatabaseName='database-5',
+            #CharacterSetName = 'string',
+            DBClusterIdentifier=db_clu,
+            DBClusterParameterGroupName='default.aurora-mysql5.7',
+            VpcSecurityGroupIds=['default'],
+            DBSubnetGroupName='db-subnet',
+            Engine='aurora-mysql',
+            EngineVersion='5.7.mysql_aurora.2.10.2',
+            Port=3306,
+            MasterUsername='admin',
+            MasterUserPassword='password',
+            OptionGroupName='default:aurora-mysql-5-7',
+            ReplicationSourceIdentifier='arn:aws:rds:us-east-1:760451896171:cluster:database-3',
+            StorageEncrypted=True,
+            KmsKeyId='dfc76317-d847-4a42-b8c7-1c17ffadde02',
+            EnableCloudwatchLogsExports=['general'],
+            EngineMode='provisioned',
+            EnableIAMDatabaseAuthentication=False,
+            DeletionProtection=False,
+            GlobalClusterIdentifier=list_db,
+                #Domain='string',
                     #DomainIAMRoleName='string',
                     #EnableGlobalWriteForwarding=True|False,
-                    DBClusterInstanceClass='db.r5.large',
-                    AllocatedStorage=100,
-                    StorageType='io1',
-                    Iops=2000,
-                    PubliclyAccessible=False,
-                    AutoMinorVersionUpgrade=True,
-                    MonitoringInterval=0,
-                    EnablePerformanceInsights=False,
-                    NetworkType='IPV4',
-                    SourceRegion='us-east-1'
-                )
-                print("Congratulations, Cluster has been created!!!")
+            DBClusterInstanceClass='db.r5.large',
+            AllocatedStorage=100,
+            StorageType='io1',
+            Iops=2000,
+            PubliclyAccessible=False,
+            AutoMinorVersionUpgrade=True,
+            MonitoringInterval=0,
+            EnablePerformanceInsights=False,
+            NetworkType='IPV4',
+            SourceRegion='us-east-1'
+        )
+        print("Congratulations, Cluster has been created!!!")
         p = 0
 
 if __name__ == '__main__':
