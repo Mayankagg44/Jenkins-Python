9e9072d58b5fb7f4c381e25d5d6676ee471579f0
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-20 11:28:59 +0530

    Update delete-cluster.py

--- delete-cluster.py
+++ delete-cluster.py
@@ -26,9 +26,9 @@ def remove_global_clusters():
             if i['Status'] == 'available':
                 response = client.remove_from_global_cluster(
                     GlobalClusterIdentifier=i['GlobalClusterIdentifier'],
-                    DbClusterIdentifier='arn:aws:rds:ca-central-1:760451896171:cluster:db-global-cluster-1'
+                    DbClusterIdentifier='arn:aws:rds:eu-west-1:760451896171:cluster:db-global-cluster-1'
                 )
-#                 print("DB".format())
+                print("\n")
 #                 print('Removing DB cluster {0}'.format(i['DbClusterIdentifier']))
 
             elif i['Status'] == 'starting' or i['Status'] == 'stopping':
